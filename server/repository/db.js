0e4356aaec7bbe41b26f3291e68c64aaffa02854
oks110 <seokko12@naver.com>
2025-03-31 13:39:01 +0900

    commit

--- server/repository/db.js
+++ server/repository/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-    host: 'ssf-react-db.c70maeu0qcke.ap-northeast-2.rds.amazonaws.com',
+    host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'mysql1234',
